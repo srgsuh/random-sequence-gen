2f953d837c690ef7688743d20ba386f19046f180
srgsuh <srg.suhoverhov@gmail.com>
2025-07-10 11:10:18 +0300

    Refactored sequence logic: improved input validation, removed debug logs

--- sequence.ts
+++ sequence.ts
@@ -1,16 +1,24 @@
 const DEFAULTS = [7, 1, 49];
 
+const inParams = process.argv.slice(2, 5).map(x => +x);
+const params = [...inParams, ...DEFAULTS.slice(inParams.length)];
+const err = validateNumbers(params);
+if (err) {
+    console.error("Error: ", err);
+    process.exit(1);
+}
+const [n, min, max] = params;
+console.log(`Successfully generated ${n} random numbers between ${min} and ${max}:`);
+console.log(generateRandomSequence(n, min, max));
+
 function random(min: number, max: number): number {
     return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 function validateNumbers(nums: number[]):string {
     const error = nums.map((value, index)=>{
-        if (!isFinite(value)) {
-            return `Argument number ${index + 1} must be a valid number`;
-        }
-        if (value !== Math.floor(value)) {
-            return `Argument number ${index + 1} must be an integer`;
+        if (!Number.isInteger(value)) {
+            return `Argument number ${index + 1} must be an integer number`;
         }
         return "";
     }).filter(x=>x).join("\n");
@@ -25,13 +33,12 @@ function validateNumbers(nums: number[]):string {
         return `Argument number 2 must be less than or equal to the argument number 3`;
     }
     if (n > max - min + 1) {
-        return `Argument number 1 must be less than or equal to the difference between the arguments 2 and 3`;
+        return `Argument number 1 must be less than or equal to the difference between the arguments 3 and 2`;
     }
     return "";
 }
 
 function generateBySelect(n: number, min: number, max: number): number[] {
-    console.log("Generating by select");
     const length = max - min + 1;
     const numbers = Array.from({length})
         .map((_, i)=>min + i);
@@ -43,7 +50,6 @@ function generateBySelect(n: number, min: number, max: number): number[] {
 }
 
 function generateByMemoize(n: number, min: number, max: number): number[] {
-    console.log("Generating by memoize");
     const nums = new Set<number>();
     while (nums.size < n) {
         const randomNumber = random(min, max);
@@ -61,15 +67,3 @@ function generateRandomSequence(n: number, min: number, max: number): number[] {
     }
     return generateBySelect(n, min, max);
 }
-
-
-const inParams = process.argv.slice(2, 5).map(x => +x);
-const params = [...inParams, ...DEFAULTS.slice(inParams.length)];
-const err = validateNumbers(params);
-if (err) {
-    console.error("Error: ", err);
-    process.exit(1);
-}
-const [n, min, max] = params;
-console.log(`Successfully generated ${n} random numbers between ${min} and ${max}:`);
-console.log(generateRandomSequence(n, min, max));
