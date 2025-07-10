fec5e899a880b4a85837ac5548a865e06c417ccd
srgsuh <srg.suhoverhov@gmail.com>
2025-07-10 10:53:51 +0300

    Removed lodash dependency and refactored code to eliminate its usage; inlined validate and generate functions into sequence.ts. Cleaned up unused dependencies in package.json.

--- sequence.ts
+++ sequence.ts
@@ -1,18 +1,54 @@
-import {validate, generate} from "./utils";
-
 const DEFAULTS = [7, 1, 49];
 
-const params = process.argv.slice(2, 5).map(Number);
-console.log(params);
-const fullParams = (params.length === DEFAULTS.length)?
-                params:
-                [...params, ...DEFAULTS.slice(params.length)];
-console.log(fullParams);
-const errors = validate(...fullParams);
-
-if (errors.length > 0) {
-    console.error(errors);
-    process.exit(1);
+function random(min: number, max: number): number {
+    return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-console.log(...generate(...fullParams));
+function validateNumbers(nums: number[]):string {
+    const error = nums.map((value, index)=>{
+        if (!isFinite(value)) {
+            return `Argument number ${index + 1} must be a valid number`;
+        }
+        if (value !== Math.floor(value)) {
+            return `Argument number ${index + 1} must be an integer`;
+        }
+        return "";
+    }).filter(x=>x).join("\n");
+    if (error) {
+        return error;
+    }
+    const [n, min, max] = nums;
+    if (n < 1) {
+        return `Argument number 1 must be a positive integer`;
+    }
+    if (min > max) {
+        return `Argument number 2 must be less than or equal to the argument number 3`;
+    }
+    if (n > max - min + 1) {
+        return `Argument number 1 must be less than or equal to the difference between the arguments 2 and 3`;
+    }
+    return "";
+}
+
+function generateRandomSequence(n: number, min: number, max: number): number[] {
+    const length = max - min + 1;
+    const numbers = Array.from({length})
+        .map((_, i)=>min + i);
+    for (let i = 0; i < n; i++) {
+        const randomIndex = random(i, length - 1);
+        [numbers[i], numbers[randomIndex]] = [numbers[randomIndex], numbers[i]]; // all chosen numbers in the beginning
+    }
+    return numbers.slice(0, n);
+}
+
+
+const inParams = process.argv.slice(2, 5).map(x => +x);
+const params = [...inParams, ...DEFAULTS.slice(inParams.length)];
+const err = validateNumbers(params);
+if (err) {
+    console.error("Error: ", err);
+    process.exit(1);
+}
+const [n, min, max] = params;
+console.log(`Successfully generated ${n} random numbers between ${min} and ${max}:`);
+console.log(generateRandomSequence(n, min, max));
